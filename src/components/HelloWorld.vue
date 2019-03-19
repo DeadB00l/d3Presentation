3c408784ef49faf097cac1cc6919299fccf26b7f
Joseph VanWagoner <joseph.vanwagoner@chghealthcare.com>
2019-03-18 18:42:15 -0600

    Step7

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,37 +21,39 @@ export default {
     }
   },
 
-  mounted() {
-    let width = 420;
-    let barHeight = 20;
+    mounted() {
+        let width = 960,
+            height = 500;
 
-    let x = scaleLinear()
-        .domain([0, d3.max(this.data, function(d) { return d.value; })])
-        .range([0, width]);
+        let y = scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(this.data, function(d) { return d.value; })]);
 
-    let chart = d3.select(".chart")
-      .attr("width", width)
-      .attr("height", barHeight * this.data.length);
+        var chart = d3.select(".chart")
+            .attr("width", width)
+            .attr("height", height);        
 
-    var bar = chart.selectAll("g")
-        .data(this.data)
-      .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+        let barWidth = width / this.data.length;
 
-    bar.append("rect")
-        .attr("width", function(d) { return x(d.value); })
-        .attr("height", barHeight - 1);
+        let bar = chart.selectAll("g")
+            .data(this.data)
+        .enter().append("g")
+            .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
-    bar.append("text")
-        .attr("x", function(d) { return x(d.value) - 3; })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .text(function(d) { return d.value; });
-  }
+        bar.append("rect")
+            .attr("y", function(d) { return y(d.value); })
+            .attr("height", function(d) { return height - y(d.value); })
+            .attr("width", barWidth - 1);
+
+        bar.append("text")
+            .attr("x", barWidth / 2)
+            .attr("y", function(d) { return y(d.value) + 3; })
+            .attr("dy", ".75em")
+            .text(function(d) { return d.value; });
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
   .chart rect {
     fill: steelblue;
