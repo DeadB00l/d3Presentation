29ff6e27fdcf23914de447d1f402cd0ccd91294b
Joseph VanWagoner <joseph.vanwagoner@chghealthcare.com>
2019-03-18 18:41:06 -0600

    Step6

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,14 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      data: [4, 8, 42, 16, 32, 1]
+      data: [
+        {name: "Locke",    value:  4},
+        {name: "Reyes",    value:  8},
+        {name: "Ford",     value: 42},
+        {name: "Jarrah",   value: 16},
+        {name: "Shephard", value: 32},
+        {name: "Kwon",     value: 23}
+      ]
     }
   },
 
@@ -19,27 +26,27 @@ export default {
     let barHeight = 20;
 
     let x = scaleLinear()
-        .domain([0, d3.max(this.data)])
+        .domain([0, d3.max(this.data, function(d) { return d.value; })])
         .range([0, width]);
 
     let chart = d3.select(".chart")
-        .attr("width", width)
-        .attr("height", barHeight * this.data.length);
+      .attr("width", width)
+      .attr("height", barHeight * this.data.length);
 
-    let bar = chart.selectAll("g")
+    var bar = chart.selectAll("g")
         .data(this.data)
       .enter().append("g")
         .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
     bar.append("rect")
-        .attr("width", x)
+        .attr("width", function(d) { return x(d.value); })
         .attr("height", barHeight - 1);
 
     bar.append("text")
-        .attr("x", function(d) { return x(d) - 3; })
+        .attr("x", function(d) { return x(d.value) - 3; })
         .attr("y", barHeight / 2)
         .attr("dy", ".35em")
-        .text(function(d) { return d; });
+        .text(function(d) { return d.value; });
   }
 }
 </script>
